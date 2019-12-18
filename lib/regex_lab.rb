63828c39a1808de22ed19863d4db27ab73b8594f
Doak Covington <doakcovington@protonmail.com>
2019-12-18 03:00:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,13 @@
 def starts_with_a_vowel?(word)
   if word.match(/^[aeiou]|^[AEIOU]/)
     return true
+  else
+    return false
   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\bun\w+ing\b/)
 
 end
 
